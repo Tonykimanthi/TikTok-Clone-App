b2475418656950af1eeb0e7de1fd46b2075e0a92
Tony Km <kimaxhanthony9@gmail.com>
2023-11-10 22:24:57 +0300

    Footer Component

--- src/home/Footer.jsx
+++ src/home/Footer.jsx
@@ -11,11 +11,11 @@ const Footer = () => {
       <main className="px-2.5 py-2 absolute left-0 right-0 bottom-0 flex justify-between items-center">
         {/* Home/Discover */}
         <div className="flex items-center gap-x-5">
-          <button className="text-[#f5e2cb] text-sm grid place-items-center leading-4">
+          <button className="text-[#f5e2cb] text-sm grid place-items-center leading-4 hover:text-[#e9c08b] transition">
             <HomeIcon />
             Home
           </button>
-          <button className="text-[#f5e2cb] text-sm grid place-items-center leading-4">
+          <button className="text-[#f5e2cb] text-sm grid place-items-center leading-4 hover:text-[#e9c08b] transition">
             <DiscoverIcon />
             Discover
           </button>
@@ -25,11 +25,11 @@ const Footer = () => {
         </button>
         {/* Inbox/Profile */}
         <div className="flex items-center gap-x-5">
-          <button className="text-[#f5e2cb] text-sm grid place-items-center leading-4">
+          <button className="text-[#f5e2cb] text-sm grid place-items-center leading-4 hover:text-[#e9c08b] transition">
             <ChatBubbleIcon />
             Inbox
           </button>
-          <button className="text-[#f5e2cb] text-sm grid place-items-center leading-4">
+          <button className="text-[#f5e2cb] text-sm grid place-items-center leading-4 hover:text-[#e9c08b] transition">
             <UserIcon />
             Profile
           </button>
