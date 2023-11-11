81014919c1c73a244ef5c22954227a8e5c26b502
Tony Km <kimaxhanthony9@gmail.com>
2023-11-11 12:15:40 +0300

    Replace image with video

--- src/home/Feed.jsx
+++ src/home/Feed.jsx
@@ -3,12 +3,10 @@ import SidePanel from "./SidePanel";
 
 const Feed = ({ showComments, handleShowComments, commentsList }) => {
   return (
-    <main className="h-[72%] relative">
-      <img
-        className="w-full h-full"
-        src="https://images.unsplash.com/photo-1682686581797-21ec383ead02?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
-        alt="Unsplash"
-      />
+    <main className="h-[72%] relative bg-black">
+      <video className="w-full h-full" autoPlay muted>
+            <source src="https://assets.mixkit.co/videos/preview/mixkit-going-down-a-curved-highway-through-a-mountain-range-41576-large.mp4" type="video/mp4" />
+        </video>
       <NameCaption />
       <SidePanel
         showComments={showComments}
