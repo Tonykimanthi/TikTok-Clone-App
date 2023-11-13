d25fbba7c875983a792976df7cb170854edc3cde
Tony Km <kimaxhanthony9@gmail.com>
2023-11-13 17:53:57 +0300

    Video loop

--- src/home/Feed.jsx
+++ src/home/Feed.jsx
@@ -5,7 +5,7 @@ import { FaPlay } from "react-icons/fa";
 const Feed = ({ showComments, handleShowComments, commentsList, videoRef, onPause, handlePause }) => {
   return (
     <main className="h-[72%] relative bg-black">
-      <video ref={videoRef} className="w-full h-full cursor-pointer" autoPlay muted onClick={handlePause}>
+      <video ref={videoRef} className="w-full h-full cursor-pointer" autoPlay muted loop onClick={handlePause}>
         <source
           src="https://assets.mixkit.co/videos/preview/mixkit-going-down-a-curved-highway-through-a-mountain-range-41576-large.mp4"
           type="video/mp4"
