178f2c89bac0d224cb5fcae4515d29124687f07e
Tony Km <kimaxhanthony9@gmail.com>
2023-11-16 10:51:46 +0300

    Footer Component

--- src/assets/SquarePlusIcon.jsx
+++ src/assets/SquarePlusIcon.jsx
@@ -7,7 +7,7 @@ const SquarePlusIcon = () => {
       strokeLinejoin="round"
       strokeWidth="2"
       viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg" className="w-11 h-11 stroke-emerald-500">
+      xmlns="http://www.w3.org/2000/svg" className="w-11 h-11 stroke-emerald-500 hover:scale-110 transition">
       <path d="M18 4H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2Z"></path>
       <path d="M9 12h6"></path>
       <path d="M12 9v6"></path>
