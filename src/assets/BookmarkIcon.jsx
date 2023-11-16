f0a5b09f6dcf38e0b4cfae35dd4971469642bea0
Tony Km <kimaxhanthony9@gmail.com>
2023-11-16 12:07:47 +0300

    Animations

--- src/assets/BookmarkIcon.jsx
+++ src/assets/BookmarkIcon.jsx
@@ -1,6 +1,8 @@
+import { motion } from "framer-motion";
+
 const BookmarkIcon = ({ handleFavourite, favFilled }) => {
   return (
-    <svg
+    <motion.svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
@@ -10,15 +12,16 @@ const BookmarkIcon = ({ handleFavourite, favFilled }) => {
         favFilled
           ? "fill-yellow-400 stroke-yellow-400"
           : "fill-white stroke-white"
-      } cursor-pointer`}
+      } cursor-pointer outline-none`}
       onClick={handleFavourite}
+      whileTap={{scale: 1.2}}
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
       />
-    </svg>
+    </motion.svg>
   );
 };
 
--- src/assets/MainHeartIcon.jsx
+++ src/assets/MainHeartIcon.jsx
@@ -1,7 +1,8 @@
+import { motion } from "framer-motion";
+
 const HeartIcon = ({ handleLike, likeFilled }) => {
   return (
-    <svg
-      onClick={handleLike}
+    <motion.svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
@@ -9,14 +10,16 @@ const HeartIcon = ({ handleLike, likeFilled }) => {
       stroke="currentColor"
       className={`w-7 h-7 ${
         likeFilled ? "fill-pink-500 stroke-pink-500" : "fill-white stroke-white"
-      } cursor-pointer`}
+      } cursor-pointer outline-none`}
+      onClick={handleLike}
+      whileTap={{scale: 1.5}}
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
       />
-    </svg>
+    </motion.svg>
   );
 };
 
