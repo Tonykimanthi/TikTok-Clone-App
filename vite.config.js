2145e11bad9967d3c82f49d61c96a20841a489d6
Tony Km <kimaxhanthony9@gmail.com>
2023-11-19 17:05:27 +0300

    Edit:vite config & package json

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tiktok-clone/"
+  base: "/TikTok-Clone-App/"
 })
